b9f51619474eafbf14c6e30b3fc14b818f764515
Roman Rudenko <ra.rudenko@gmail.com>
2011-11-26 23:34:42 +0200

    [+] Add tests for FreerollBot

---
+++ Tests/FreerollBotTests/FreerollBotTests.cpp
@@ -0,0 +1,14 @@
+// FreerollBotTests.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "FreerollStrategy.h"
+
+#include <src/gtest-all.cc>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
